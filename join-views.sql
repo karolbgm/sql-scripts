aabb9756754f370608b1e56fdb3954956e2f3fb8
Karol Morgan <karolyne.morgan@outlook.com>
2024-06-06 12:12:20 -0400

    join views

--- join-views.sql
+++ join-views.sql
@@ -1,9 +1,38 @@
-SELECT Date, Orders.Description 'Order', Product, OrderLines.Description, Quantity, Price 
-FROM Orders
- join OrderLines on OrderLines.OrdersId = Orders.Id
- ORDER BY Product, Description ASC;
+--INNER JOIN
 
- SELECT *
-FROM Customers
- join Orders
- on Orders.CustomerId = Customers.Id;
+--SELECT Date, Orders.Description 'Order', Product, OrderLines.Description, Quantity, Price 
+--FROM Orders
+-- join OrderLines on OrderLines.OrdersId = Orders.Id
+-- ORDER BY Product, Description ASC;
+
+-- SELECT *
+--FROM Customers
+-- join Orders
+-- on Orders.CustomerId = Customers.Id;
+
+
+--ALIAS FOR TABLE NAMES
+--SELECT *
+--FROM Customers c
+--join Orders o
+--on o.CustomerId = c.Id
+--join OrderLines ol 
+--on ol.OrdersId = o.Id;
+
+--LEFT JOIN
+SELECT *
+FROM Customers c
+left join Orders o
+on o.CustomerId = c.Id;
+
+--RIGHT JOIN
+SELECT *
+FROM Customers c
+right join Orders o
+on o.CustomerId = c.Id;
+
+--OUTER JOIN
+SELECT *
+FROM Customers c
+full outer join Orders o
+on o.CustomerId = c.Id;
